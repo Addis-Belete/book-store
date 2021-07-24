291978c9d7453bdba8abeac997f2d749bd40ed1a
Addis Belete <addis.bel@gmail.com>
2021-07-24 11:57:36 +0300

    Add BookList component

---
+++ src/components/BooksList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookList = () => (
+  <table>
+    <tr>
+      <th>Book ID</th>
+      <th>Title</th>
+      <th>Category</th>
+
+    </tr>
+
+  </table>
+);
+
+export default BookList;
