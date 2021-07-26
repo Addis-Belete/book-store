687ce2b4f32de9fb33acbccef9e90561a893b2e9
Addis Belete <addis.bel@gmail.com>
2021-07-26 18:04:26 +0300

    Add state to Bookform

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,12 +1,30 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
+import React, { useState } from 'react';
 
 const BooksForm = () => {
+  const [setBook] = useState({
+    title: '',
+    category: '',
+  });
+
+  const handleChange = (e) => {
+    if (e.target.id === ' book-tilt') {
+      setBook({
+        title: e.target.innerText,
+      });
+    } else if (e.target.id === 'book-category') {
+      setBook({
+        category: e.target.innerText,
+
+      });
+    }
+  };
+
   const bookCategory = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   return (
     <div className="book-form">
-      <input type="text" placeholder="Add title" />
-      <select name="Book Category" id="select">
+      <input type="text" id="book-title" placeholder="Add title" onChange={(e) => handleChange(e.target.innerText)} />
+      <select name="Book-Category" id="select">
         {bookCategory.map((x, i) => (
           <option key={i}>{x}</option>
         ))}
