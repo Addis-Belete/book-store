dab97d139a7deaa9567382021efaa93e6d91e99f
Addis Belete <addis.bel@gmail.com>
2021-07-28 10:37:52 +0300

    Render Category filter in BookList

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
-import CategoryFilter from './CategoryFilter';
 
 const App = () => (
   <div className="App">
     <BooksForm />
-    <CategoryFilter />
+
     <BooksList />
   </div>
 );
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,13 +1,12 @@
 /* eslint-disable array-callback-return */
 import React from 'react';
 
-const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const category = ['All', ' Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
 const CategoryFilter = () => (
   <select name="filter" id="filter">
     {category.map((cat) => ((
-      <option value="all">All</option>,
-        <option value={cat}>{cat}</option>
+      <option key={cat} value={cat}>{cat}</option>
     )))}
 
   </select>
