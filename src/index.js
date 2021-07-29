4cb17c4f37262064cc00fc74938a0ebd3bf89d8f
Addis Belete <addis.bel@gmail.com>
2021-07-29 15:20:01 +0300

    Import google fonts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './reducers/index';
-import './fonts/Montserrat/Montserrat-Bold.ttf';
 
 ReactDOM.render(
   <Provider store={store}>
